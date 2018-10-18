26308ac046d4ef74d0857ac24a9bb9f859b22a10
Mike Henry <chsmike@gmail.com>
2018-10-18 18:24:09 -0400

    Revert "999 Test retreival of local storage from Core Data"
    This reverts commit f7b9a6947af8012850263593950fa14fef34fbb4.

--- RocketFancy/RocketFancy/ViewController.swift
+++ RocketFancy/RocketFancy/ViewController.swift
@@ -7,36 +7,22 @@
 //
 
 import UIKit
-import CoreData
 
 class ViewController: UIViewController {
     
     //MARK: - Properties
     let networkManager = NetworkManager.sharedInstance
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     var launchesArray = [Launch]()
 
     
     //MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        let context = self.appDelegate.persistentContainer.viewContext
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Launch")
-        request.returnsObjectsAsFaults = false
-        
-        do {
-            let result = try context.fetch(request)
-            for data in result as! [NSManagedObject] {
-                print(data.value(forKey: "flightNumber") as! Int16)
+        networkManager.fetchData { (launches) in
+            if let unwrappedLaunches = launches {
+                self.launchesArray = unwrappedLaunches
             }
-        } catch {
-            print("Core Data retrival failure")
-            
-//        networkManager.fetchData { (launches) in
-//            if let unwrappedLaunches = launches {
-//                self.launchesArray = unwrappedLaunches
-//            }
-//            print("Count: \(self.launchesArray.count)")
+            print("Count: \(self.launchesArray.count)")
         }
         
     }
